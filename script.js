fc0a93f486e592f7c7957f9cdcb82510a616bff2
Ross Henderson <rossliamhenderson@gmail.com>
2022-07-18 22:20:17 +0100

    Finished up with basic styling.
    Made a start on interactivity - added different color for hovering on the
    number buttons.

---
+++ script.js
@@ -0,0 +1,95 @@
+let one = document.getElementById('one');
+
+one.addEventListener('mouseover', ()=> {
+    one.style.backgroundColor = 'white';});
+
+    one.addEventListener('mouseout', ()=> {
+        one.style.backgroundColor='rgb(206, 206, 206)'});
+
+
+
+
+
+    let two = document.getElementById('two');
+
+two.addEventListener('mouseover', ()=> {
+    two.style.backgroundColor = 'white';});
+
+    two.addEventListener('mouseout', ()=> {
+        two.style.backgroundColor='rgb(206, 206, 206)'});
+
+
+
+    let three = document.getElementById('three');
+
+    three.addEventListener('mouseover', ()=> {
+        three.style.backgroundColor = 'white';});
+    
+        three.addEventListener('mouseout', ()=> {
+            three.style.backgroundColor='rgb(206, 206, 206)'});
+    
+
+
+    let four = document.getElementById('four');
+
+    four.addEventListener('mouseover', ()=> {
+        four.style.backgroundColor = 'white';});
+    
+        four.addEventListener('mouseout', ()=> {
+            four.style.backgroundColor='rgb(206, 206, 206)'});
+    
+
+
+    let five = document.getElementById('five');
+
+    five.addEventListener('mouseover', ()=> {
+        five.style.backgroundColor = 'white';});
+    
+        five.addEventListener('mouseout', ()=> {
+            five.style.backgroundColor='rgb(206, 206, 206)'});
+    
+
+
+
+    let six = document.getElementById('six');
+
+    six.addEventListener('mouseover', ()=> {
+        six.style.backgroundColor = 'white';});
+    
+        six.addEventListener('mouseout', ()=> {
+            six.style.backgroundColor='rgb(206, 206, 206)'});
+    
+
+
+
+    let seven = document.getElementById('seven');
+
+    seven.addEventListener('mouseover', ()=> {
+        seven.style.backgroundColor = 'white';});
+    
+        seven.addEventListener('mouseout', ()=> {
+            seven.style.backgroundColor='rgb(206, 206, 206)'});
+    
+
+
+
+    let eight = document.getElementById('eight');
+
+    eight.addEventListener('mouseover', ()=> {
+        eight.style.backgroundColor = 'white';});
+    
+        eight.addEventListener('mouseout', ()=> {
+            eight.style.backgroundColor='rgb(206, 206, 206)'});
+    
+
+
+
+    let nine = document.getElementById('nine');
+
+    nine.addEventListener('mouseover', ()=> {
+        nine.style.backgroundColor = 'white';});
+    
+        nine.addEventListener('mouseout', ()=> {
+            nine.style.backgroundColor='rgb(206, 206, 206)'});
+    
+    
