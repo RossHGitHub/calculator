f8e69ca87aee14c32844f8e089b150a0d0444418
Ross Henderson <rossliamhenderson@gmail.com>
2022-07-20 23:05:55 +0100

    HARD U-turn on the div idea.
    I subsequently learned a lot about DOM manipulation and what NOT to do.
    So time spent learing is not wasted!
    
    Changed the DIVs to Button elements.
    
    Created HTML Data attributes for all buttons to reference in JS.
    
    Started creating the Calculator class with some methods, currently trying
    to get the numbers to display on the display when clicked.
    
    Succesfully managed to get an '[OBJECT HMTL DIV ELEMENT] to go there,
    
    so its 50% of the way there!!

--- script.js
+++ script.js
@@ -1,191 +1,46 @@
+class Calculator {
+
+    constructor(output) {
+        this.output = output;
+    }
+
+    clear() {
+        this.output = '';
+        this.operation = undefined;
+    }
+
+    appendNumber(number) {
+        this.appendNumber.output = number;
+    }
 
 
-//ONE
-//ONE
+    chooseOperation(operation) {
 
-let one = document.getElementById('one');
+    }
 
-one.addEventListener('mouseover', ()=> {
-    one.style.backgroundColor = 'white';});
+    compute() {
 
-    one.addEventListener('mouseout', ()=> {
-        one.style.backgroundColor='rgb(206, 206, 206)'});
+    }
 
 
+    updateDisplay() {
+        this.output.innerText = this.output
+    }
+}
 
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const clearButton = document.querySelector('[data-number]');
 
-//TWO
-//TWO
+const output = document.querySelector('[data-output]');
 
-    let two = document.getElementById('two');
+const calculator = new Calculator(output);
 
-two.addEventListener('mouseover', ()=> {
-    two.style.backgroundColor = 'white';});
-
-    two.addEventListener('mouseout', ()=> {
-        two.style.backgroundColor='rgb(206, 206, 206)'});
-
-
-//THREE
-//THREE
-
-    let three = document.getElementById('three');
-
-    three.addEventListener('mouseover', ()=> {
-        three.style.backgroundColor = 'white';});
-    
-        three.addEventListener('mouseout', ()=> {
-            three.style.backgroundColor='rgb(206, 206, 206)'});
-    
-
-//FOUR
-//FOUR
-
-    let four = document.getElementById('four');
-
-    four.addEventListener('mouseover', ()=> {
-        four.style.backgroundColor = 'white';});
-    
-        four.addEventListener('mouseout', ()=> {
-            four.style.backgroundColor='rgb(206, 206, 206)'});
-    
-
-//FIVE
-//FIVE
-
-    let five = document.getElementById('five');
-
-    five.addEventListener('mouseover', ()=> {
-        five.style.backgroundColor = 'white';});
-    
-        five.addEventListener('mouseout', ()=> {
-            five.style.backgroundColor='rgb(206, 206, 206)'});
-    
-
-
-//SIX
-//SIX
-
-    let six = document.getElementById('six');
-
-    six.addEventListener('mouseover', ()=> {
-        six.style.backgroundColor = 'white';});
-    
-        six.addEventListener('mouseout', ()=> {
-            six.style.backgroundColor='rgb(206, 206, 206)'});
-    
-
-
-//SEVEN
-//SEVEN
-
-    let seven = document.getElementById('seven');
-
-    seven.addEventListener('mouseover', ()=> {
-        seven.style.backgroundColor = 'white';});
-    
-        seven.addEventListener('mouseout', ()=> {
-            seven.style.backgroundColor='rgb(206, 206, 206)'});
-    
-
-
-//EIGHT
-//EIGHT
-
-    let eight = document.getElementById('eight');
-
-    eight.addEventListener('mouseover', ()=> {
-        eight.style.backgroundColor = 'white';});
-    
-        eight.addEventListener('mouseout', ()=> {
-            eight.style.backgroundColor='rgb(206, 206, 206)'});
-    
-
-
-//NINE
-//NINE
-
-    let nine = document.getElementById('nine');
-
-    nine.addEventListener('mouseover', ()=> {
-        nine.style.backgroundColor = 'white';});
-    
-        nine.addEventListener('mouseout', ()=> {
-            nine.style.backgroundColor='rgb(206, 206, 206)'});
-    
-    
-//CLEAR
-//CLEAR
-    
-    let clear = document.getElementById('clear');
-
-    clear.addEventListener('mouseover', ()=> {
-    clear.style.backgroundColor = 'rgb(141, 141, 192)';});
-            
-    clear.addEventListener('mouseout', ()=> {
-    clear.style.backgroundColor='rgb(101, 101, 180)'});
-
-
-//ADD
-//ADD
-
-    let add = document.getElementById('add');
-
-    add.addEventListener('mouseover', ()=> {
-    add.style.backgroundColor = 'rgb(221, 186, 158)';});
-                 
-    add.addEventListener('mouseout', ()=> {
-    add.style.backgroundColor='rgb(230, 172, 124)'});
-
-
-
-//MINUS
-//MINUS
-
-    let minus = document.getElementById('minus');
-
-    minus.addEventListener('mouseover', ()=> {
-    minus.style.backgroundColor = 'rgb(221, 186, 158)';});
-                 
-    minus.addEventListener('mouseout', ()=> {
-    minus.style.backgroundColor='rgb(230, 172, 124)'});
-
-
-
-//DIVIDE
-//DIVIDE
-
-    let divide = document.getElementById('divide');
-
-    divide.addEventListener('mouseover', ()=> {
-    divide.style.backgroundColor = 'rgb(221, 186, 158)';});
-                 
-    divide.addEventListener('mouseout', ()=> {
-    divide.style.backgroundColor='rgb(230, 172, 124)'});
-
-
-
-//TIMES
-//TIMES
-
-    let times = document.getElementById('times');
-
-    times.addEventListener('mouseover', ()=> {
-    times.style.backgroundColor = 'rgb(221, 186, 158)';});
-                 
-    times.addEventListener('mouseout', ()=> {
-    times.style.backgroundColor='rgb(230, 172, 124)'});
-
-
-
-//EQUALS
-//EQUALS
-
-    let equals = document.getElementById('equals');
-
-    equals.addEventListener('mouseover', ()=> {
-    equals.style.backgroundColor = 'rgb(177, 231, 119)';});
-                 
-    equals.addEventListener('mouseout', ()=> {
-    equals.style.backgroundColor='rgb(89, 235, 69)'});
+numberButtons.forEach(button => {
+    button.addEventListener('click', ()=> {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
 
