5b008141e8f52576db951406d44fc82fc3525eaf
Ross Henderson <rossliamhenderson@gmail.com>
2022-07-22 23:14:32 +0100

    Worked out why  display wasn't updating.
    I was trying to update the div, and not the text-element so to speak, so
    had to declare the Current/Previous operands for that.
    
    Also added the clear method to the calculator constructor so that I start
    with a blank slate everytime.

--- script.js
+++ script.js
@@ -1,46 +1,89 @@
 class Calculator {
 
-    constructor(output) {
-        this.output = output;
+    constructor(current, prev) {
+        this.current = current;
+        this.prev = prev;
+        this.clear()
     }
 
     clear() {
-        this.output = '';
+        this.currentOperand = '';
+        this.previousOperand = '';
         this.operation = undefined;
+        this.updateDisplay()
     }
 
     appendNumber(number) {
-        this.appendNumber.output = number;
+     this.currentOperand = this.currentOperand.toString() + number.toString()
     }
 
 
     chooseOperation(operation) {
+    if (operation === ''){
+    return}
+
+    if (previousOperand !== '') {
+        this.compute()
+    }
+
+    this.operation = operation
+    this.previousOperand = this.currentOperand
+    this.currentOperand = '';
+
 
     }
 
     compute() {
+        
+        let computation
+        const previous = parseFloat(this.previousOperand)
+        const current = parseFloat(this.currentOperand)
+        if (isNaN(prev) || isNaN(current)) {return}
 
+        switch
+
+        
     }
 
+    updateDisplay() {
+        this.current.innerText = this.currentOperand
+        this.prev.innerText = this.previousOperand
 
-    updateDisplay(text) {
-        this.output.innerText = text
-    }
+        }
 }
 
+const current = document.querySelector('[data-current-num]');
+const prev = document.querySelector('[data-prev-num')
 const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operation]');
 const equalsButton = document.querySelector('[data-equals]');
-const clearButton = document.querySelector('[data-number]');
+const clearButton = document.querySelector('[data-all-clear]');
 
 const output = document.querySelector('[data-output]');
 
-const calculator = new Calculator(output)
+const calculator = new Calculator(current, prev)
 
 numberButtons.forEach(button => {
     button.addEventListener('click', ()=> {
-        calculator.appendNumber(button.innerText)
-        calculator.updateDisplay(button.innerText);
+        calculator.appendNumber(button.innerText);
+       calculator.updateDisplay(button.innerText);
+})
 })
 
+clearButton.addEventListener('click', () => {
+    calculator.clear();
+})
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', ()=> {
+    calculator.chooseOperation(button.innerText)
+    calculator.updateDisplay()
+
+
+    })
+})
+
+equalsButton.addEventListener('click', () => {
+    calculator.compute()
+    calculator.updateDisplay()
 })
